ead25e5b42deb3668604386a3bb5dc3d1de991c9
Estelaromero1314 <157331722+Estelaromero1314@users.noreply.github.com>
2024-05-01 07:48:09 +0000

    Version 0 -Start-Play-GameOver

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 /**
  * Project 3 versions 0-4 - 2D Web Game
- * Name:
+ * Name: Estela Romero
  * 
  * Use this template to get started creating a simple 2D game for the web using P5.js. 
  */
-
+var gameState = "splash"; // gamestate to splash
 function setup() {
 
   createCanvas(600, 400);
@@ -13,12 +13,26 @@ function setup() {
 
 function draw() {
   background(200);
-  /* un-comment each line to see it work */
-  //splash(); // call the splash screen function (below)
-  //play(); // call the play screen function (below)
-  //gameOver(); // call the gameOver screen function (below)
 
+  switch (gameState)  {
+    case "splash" :
+      splash(); // go to the "splash" screen
+      break;
+    case "play" :
+      play(); // go to the "play" screen
+      break;
+    case "gameOver" :
+      gameOver(); // go to the "game over" screen
+      break;
+    default :
+      console.log("no match found - check your mousePressed() function!");
+  
+  }
+  
+ 
 }
+
+
 
 function splash() {
   // this is what you would see when the game starts
@@ -52,5 +66,13 @@ function gameOver() {
 function mousePressed() {
 
   console.log("click!");
-
+  if(gameState == "splash") { 
+    gameState = "play"; 
+} // go to "play"
+else if(gameState == "play") { 
+    gameState = "gameOver"; 
+} // go to "gameOver"
+else if(gameState == "gameOver") { 
+    gameState = "splash"; 
+} // go to "splash"
 }
